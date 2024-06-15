508bd63925844ef9816e2608057caefbea27a7c0
Martin Nester <nesterms@dukes.jmu.edu>
2024-06-15 16:28:59 -0400

    integrate velodyne

--- compose.sh
+++ compose.sh
@@ -1,2 +1,6 @@
+# do this to see gui applications from docker
 xhost local:root
-sudo docker compose -f ./compose.yaml up --force-recreate
+# assign static ip to recieve velodyne data
+ip addr add 192.168.1.254/24 dev enp0s31f6
+# start with docker
+sudo docker compose -f ./compose.yaml up --force-recreate --build
