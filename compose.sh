5310a65df459cd11e0c9c9f4a6cc50a9dc2d5c0d
Martin Nester <nesterms@dukes.jmu.edu>
2024-06-15 01:27:48 -0400

    install script for nvidia container toolkit

---
+++ compose.sh
@@ -0,0 +1,2 @@
+xhost local:root
+sudo docker compose -f ./compose.yaml up --force-recreate
---
+++ install_container_toolkit.sh
@@ -0,0 +1,28 @@
+# SOURCE: https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html
+# PURPOSE: Setup Nvidia Container Toolkit so containers can use the nvidia resources such as CUDA. Might be used for something like zed camera object detection.
+# --------------------------------------------------------------------------------------------------
+
+if sudo docker info | grep -q Runtimes:.*nvidia
+then
+	echo "INFO: Skipping Nvidia Container Toolkit install because docker nvidia runtime already exists."
+else
+	echo "INFO: Attempting to install Nvidia Container Toolkit."
+	# Installing with Apt 
+	curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+	  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+	    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+	    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list # configure production repo
+	sudo apt-get update
+	sudo apt-get install -y nvidia-container-toolkit
+
+	# Configuring Docker
+	sudo nvidia-ctk runtime configure --runtime=docker
+	sudo systemctl restart docker
+
+	if sudo docker info | grep -q Runtimes:.*nvidia
+	then
+		echo "INFO: Nvidia Container Toolkit installed!"
+	else
+		echo "INFO: Failed to install Nvidia Container Toolkit. See https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html"
+	fi
+fi
