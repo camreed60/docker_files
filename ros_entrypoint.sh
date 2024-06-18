c5f5a7b97a45bd18e84f787955c9acb260619670
Martin Nester <nesterms@dukes.jmu.edu>
2024-06-18 12:03:26 -0400

    use correct script

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # setup ros2 environment
 source "/opt/ros/humble/setup.bash" --
-source "/dev_ws/install/local_setup.bash" --
+source "/dev_ws/install/setup.bash" --
 
 # take in command line input and execute
 echo "Successfully started JACart container. You can run a ros2 launch here."
