42cc8a00f0070a683faa4b5b34a055c807e65a7a
Martin Nester <nesterms@dukes.jmu.edu>
2024-07-03 15:14:46 -0400

    configure all ethernet interfaces instead

--- init_jacart.sh
+++ init_jacart.sh
@@ -2,17 +2,10 @@
 IMAGE_NAME=jacart
 CONTAINER_NAME=jacart
 
-# check for enviroment variables
-if [ -z "${VELODYNE_IFNAME}" ]; then
-  echo "The enviroment variable \$VELODYNE_IFNAME needs to be set to the ethernet interface that the velodyne is connected to. This is likely the logical name of the network which has a pci bus, look for it by running \`lshw -C network\`." >&2
-  return
-fi
-# install Nvidia Container Toolkit
-source ./install_container_toolkit.sh
 # do this to see gui applications (rviz2) from docker
 sudo xhost local:root
-# assign static ip to recieve velodyne data. For more information see page 23, 4.2.1 Network Setup in Isolation of the Velodyne Puck (VLP-16) User Manual (https://velodynelidar.com/wp-content/uploads/2019/12/63-9243-Rev-E-VLP-16-User-Manual.pdf).
-sudo ip addr add 192.168.1.254/24 dev $VELODYNE_IFNAME
+# assign static ip (to all ethernet interfaces) to recieve velodyne data. For more information see page 23, 4.2.1 Network Setup in Isolation of the Velodyne Puck (VLP-16) User Manual (https://velodynelidar.com/wp-content/uploads/2019/12/63-9243-Rev-E-VLP-16-User-Manual.pdf).
+sudo ip addr add 192.168.1.254/24 dev $(nmcli device status | awk '$2 == "ethernet" {print $1}') 
 # start with docker
 sudo docker build -t $IMAGE_NAME -f ./Dockerfile . 
 
