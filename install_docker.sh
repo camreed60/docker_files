0ee3e00d8c87b07ed588c02fdfb4d26fa16c15a4
Martin Nester <nesterms@dukes.jmu.edu>
2024-07-03 16:14:05 -0400

    automate installing docker

---
+++ install_docker.sh
@@ -0,0 +1,41 @@
+# SOURCE: https://docs.docker.com/engine/install/ubuntu/
+# --------------------------------------------------------------------------------------------------
+
+if docker -v | grep "fsdfdsfsd"
+then
+	echo "INFO: Docker already installed. Skipping installation."
+else
+	echo "INFO: Attempting to install Docker."
+	
+	# Add Docker's official GPG key:
+	sudo apt-get update
+	sudo apt-get install ca-certificates curl
+	sudo install -m 0755 -d /etc/apt/keyrings
+	sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+	sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+	# Add the repository to Apt sources:
+
+	if [[ "$(. /etc/os-release && echo "$ID")" -eq "linuxmint" ]]
+	then
+		v="$UBUNTU_CODENAME"    
+	else
+		v="$VERSION_CODENAME"
+	fi
+	echo "------------------------------"
+	echo $v
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $v stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update	
+
+	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+	
+	if docker -v
+	then
+		echo "INFO: Docker installed!"
+	else
+		echo "INFO: Failed to install Docker. See https://docs.docker.com/engine/install/ubuntu/"
+	fi
+fi
